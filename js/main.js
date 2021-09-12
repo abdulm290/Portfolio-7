6b44411e6e0e8803d466afc1ad53348b6751a4aa
Abdul Malik <ammalik290@gmail.com>
2021-09-12 08:50:02 +0600

    food website

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.single-item').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        focusOnSelect: true,
+        arrows: false,
+        autoplay: true,
+      });
+  });
